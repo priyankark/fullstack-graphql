ea71b0bb573b9a55a4b794ca083984fa2ad576eb
Priyankar Kumar <priyankar.kumar98@gmail.com>
2019-12-15 17:23:17 +0530

    Server side Graph QL

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,40 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    // demo(_,__,{models}) {
+    //   models.Pet.findMany({})
+
+    // },
+
+    pets(_,{input},ctx,info) {
+      return ctx.models.Pet.findMany(input); 
+      //return p.filter((inp)=>input.type===p.type);
+    } ,
+    pet(_,{input},ctx) {
+      return ctx.models.Pet.findOne(input); 
+      //return p.filter((inp)=>input.type===p.type);
     }
   },
-  User: {
-    
-  }
+  Mutation: {
+    newPet(_,{input}, ctx){
+      const pet =ctx.models.Pet.create(input);
+      return pet;
+    }
+  },
+   Pet: {
+     owner(pet,_,ctx) {
+       return ctx.models.User.findOne({});
+
+     }
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  },
+   User: {
+    pets(user,_,ctx) {
+      return ctx.models.Pet.findMany({});
+    }  
+   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,38 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+    type User {
+       id: ID
+       username: String!
+       pets: [Pet]
+    } 
 
+    type Pet {
+         id: ID
+         createdAt: String!
+         name: String!
+         type: String!
+         owner: User!
+    }
+
+    input PetInput {
+        name: String
+        type: String
+    }
+
+    type Query {
+        pets(input: PetInput): [Pet]!
+        pet(input: PetInput): Pet 
+    }
+
+    input newPetInput{
+        name: String
+        type: String
+    }
+
+    type Mutation {
+        newPet(input: newPetInput): Pet!
+    }
 `;
 
 module.exports = typeDefs
